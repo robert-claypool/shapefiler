96e98d2df763fd37998de0f7cedcce0d5f524dee
Robert Claypool <robert.g.claypool+git@gmail.com>
2015-11-26 10:00:21 -0600

    Add browserify, move shp-write code to app.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 'use strict';
 
 var gulp = require('gulp');
-var browserify = require('browserify');
 var html5Lint = require('gulp-html5-lint');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+// var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
 
 gulp.task('html5-lint', function() {
     return gulp.src('*.html')
         .pipe(html5Lint());
 });
 
-gulp.task('default', ['html5-lint']);
+gulp.task('js', function() {
+    var b = browserify({
+        entries: './app.js',
+        debug: true
+    });
+
+    return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    // .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('default', ['html5-lint', 'js']);
